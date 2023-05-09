d53c4aa2876bc88f5ed0dbcc5c70fcb2c65453d0
ProstoYosip <106190672+ProstoYosip@users.noreply.github.com>
2023-05-09 11:20:14 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,15 @@
-if($(".list__item").css("display") === "none"){
-$("i").on("click", function(){
-		$(".list__item").fadeToggle();
-});
-} else{}
+// if($(".list__item").css("display") === "none"){
+// $("i").on("click", function(){
+// 		$(".list__item").fadeToggle();
+// });
+// } else{}
+$(".list__items").click(function(){
+	if($(".list__item").css("display") === "none"){
+		$(".list__item").fadeIn();
+	} else{
+		$(".list__item").fadeOut();
+	}
+})
 
 
 
